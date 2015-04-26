62652192fe0e940a0057ce77a7af2ce65934946e
cle <cle@linuxguru.fedora>
2015-04-26 18:34:04 +0200

    marshal complex object

--- JAXB/src/test/java/org/jaxb/JAXBTest.java
+++ JAXB/src/test/java/org/jaxb/JAXBTest.java
@@ -1,71 +1,67 @@
 package org.jaxb;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.Writer;
+import java.io.FileOutputStream;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.MarshalException;
 import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
 
 import org.apache.commons.io.FileUtils;
+import org.jaxb.generated.ColorType;
+import org.jaxb.generated.ConfigurationType;
+import org.jaxb.generated.ObjectFactory;
+import org.jaxb.generated.UiSettingsType;
 import org.junit.Test;
 
 public class JAXBTest {
-	
-	@XmlAccessorType(XmlAccessType.FIELD)
-	@XmlType(name = "configuration", propOrder = { "screenName", "webProxy",
-			"verbose", "colorName" })
-	@XmlRootElement(name = "config")
-	static class Configuration {
-		private String webProxy;
-		private boolean verbose;
-		private String colorName;
-		private String screenName;
-
-		public String getColorName() {
-			return colorName;
-		}
-
-		public void setColorName(String colorName) {
-			this.colorName = colorName;
-		}
-	}
 
 	@Test
 	public void testMarshall() throws Exception {
 		final File f = new File("test.xml");
 		assertFalse("File doesn't exist yet ", f.exists());
-		Configuration configuration = new Configuration();
-		configuration.setColorName("blue");
-		JAXBContext jc = JAXBContext.newInstance(Configuration.class);
+		ConfigurationType configuration = createConfigurationType();
+		ObjectFactory factory = new ObjectFactory();
+		JAXBContext jc = JAXBContext.newInstance(ConfigurationType.class);
 		Marshaller saver = jc.createMarshaller();
-
-		Writer saveFile = new FileWriter(f);
-		saver.marshal(configuration, saveFile);
-		saveFile.close();
-
+		try(FileOutputStream fos = new FileOutputStream(f)) {
+			saver.marshal(factory.createConfiguration(configuration), fos);
+		}
 		assertTrue("File exists now ", f.exists());
-
 		FileUtils.forceDeleteOnExit(f);
 	}
 
-	@Test
-	public void testUnMarshall() throws Exception {
-		final File f = FileUtils.toFile(this.getClass().getResource(
-				"/toUnmarshall.xml"));
-		JAXBContext jc = JAXBContext.newInstance(Configuration.class);
-		Unmarshaller unmarshaller = jc.createUnmarshaller();
+	/**
+	 * directly marshal using java object. Not register via ObjectFactory
+	 * @throws Exception
+	 */
+	@Test(expected=MarshalException.class)
+	public void testMarshall_Failed() throws Exception {
+		final File f = new File("test.xml");
+		assertFalse("File doesn't exist yet ", f.exists());
+		ConfigurationType configuration = createConfigurationType();
+		JAXBContext jc = JAXBContext.newInstance(ConfigurationType.class);
+		Marshaller saver = jc.createMarshaller();
+		try(FileOutputStream fos = new FileOutputStream(f)) {
+			saver.marshal(configuration, fos);
+		}finally{
+			FileUtils.forceDeleteOnExit(f);
+		}
+	}
 
-		Configuration configuration = (Configuration) unmarshaller.unmarshal(f);
-		assertNotNull(configuration);
-		assertEquals("red", configuration.getColorName());
+	private ConfigurationType createConfigurationType() {
+		ConfigurationType configuration = new ConfigurationType();
+		UiSettingsType uiSettings = new UiSettingsType();
+		ColorType backGroundColor = new ColorType();
+		backGroundColor.setBlue(10);
+		backGroundColor.setGreen(11);
+		backGroundColor.setRed(14);
+		uiSettings.setBackgroundColor(backGroundColor);
+		configuration.setUiSettings(uiSettings);
+		return configuration;
 	}
 
 }
